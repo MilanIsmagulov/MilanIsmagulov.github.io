e472eea322fc4dc2b22774ee580267506904c428
MilanIsmagulov <4k.milan15@gmail.com>
2023-04-19 09:19:28 +0500

    44strbook

--- FirstPythonApp.py
+++ FirstPythonApp.py
@@ -1,1 +1,17 @@
-print ('Hello World')
+import math
+print ('Hello World') #Just Hello World!!!
+a = (2+3)/0.25-4*2.1
+print (a)
+txt = "Язык программирования Питон"
+print (txt)
+num = 123
+print ("Целое число:", num)
+value = "язык Python"
+print ("Текст:", value)
+value = 321
+print ("Число:", value)
+name = input ("Как вас зовут?")
+age = int(input("Сколько вам лет?"))
+#Отображение считанных значений
+print ("Добрый день,",name+ "!")
+print ("Вы родились в", 2023-age, "году.")
